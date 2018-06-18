e700e6b880709db86d0f5018eaf82810d7bf6fb9
KarinaMealey <kem32002@gmail.com>
2018-06-18 21:15:14 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,8 +1,10 @@
 # Code your prompts here!
 
 # Try starting out with puts'ing a string.
-guest_name=Hermione
-party_name=Halloween
-date=
-time=
-host_name=
+guest_name = "Hermione"
+party_name = "Halloween"
+date = "October 31"
+time = "6pm"
+host_name = "Harry Potter"
+
+puts "Dear #{guest_name},"
