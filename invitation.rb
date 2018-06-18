b5b56a8d507f3198db41b341c66a48ab80a8c4bc
KarinaMealey <kem32002@gmail.com>
2018-06-18 21:17:04 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -8,4 +8,5 @@ time = "6pm"
 host_name = "Harry Potter"
 
 puts "Dear #{guest_name},"
-puts "You are invited to my #{party_name} Party on #{date} at #{time}"
+puts "You are invited to my #{party_name} Party on #{date} at #{time}."
+puts "Sincerely #{host_name}"
